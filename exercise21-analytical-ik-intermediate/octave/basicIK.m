e3888b5441c2aa0da03bb88b9dccb3a5d264f705
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2024-02-27 23:44:04 -0800

    Dummy files

---
+++ exercise21-analytical-ik-intermediate/octave/basicIK.m
@@ -0,0 +1,48 @@
+TODO convert to octave
+
+% Analytical Inverse Kinematics with Symbols in 4-DOF (Matlab)
+
+% Define Joint Variables as Symbols
+% If you don't declare them as "real", you may get imaginary numbers
+syms base shoulder elbow wrist real
+
+% End-Effector Pose from IK
+EE = ...
+  zrotate(-0.528585) * ... % base
+  translate(0, 0, 0.670) * ...
+  yrotate(-0.585409) * ... % shoulder
+  translate(0.7, 0, 0) * ...
+  yrotate(1.23597) * ...   % elbow
+  translate(0.7, 0.05, 0) * ...
+  xrotate(0.917576) * ...  % wrist
+  translate(0.18, 0, 0);
+
+% Define Joint Frames
+Base = zrotate(base);
+Shoulder = ...
+  translate(0, 0, 0.670) * ...
+  yrotate(shoulder);
+Elbow = ...
+  translate(0.7, 0, 0) * ...
+  yrotate(elbow);
+Wrist = translate(0.7, 0.05, 0) * ...
+  xrotate(wrist) * ...
+  translate(0.18, 0, 0);
+
+% Define IK Equation
+IK = Base * Shoulder * Elbow * Wrist == EE;
+
+% Decouple Wrist Frame
+IK = Base * Shoulder * Elbow == EE * inv(Wrist);
+
+% Decouple Elbow Frame
+IK = Base * Shoulder == EE * inv(Wrist) * inv(Elbow);
+
+% Solve for Joint Variables
+[base, shoulder, elbow, wrist] = solve(IK, [base, shoulder, elbow, wrist])
+
+% Print Results
+vpa(base)
+vpa(shoulder)
+vpa(elbow)
+vpa(wrist)
---
+++ exercise21-analytical-ik-intermediate/octave/str1kerIK.m
@@ -0,0 +1,41 @@
+TODO convert to Octave
+
+% Analytical Inverse Kinematics with Symbols in 3-DOF (Matlab)
+
+% Define Joint Variables
+% If you don't declare them as "real", you may get imaginary numbers
+syms base shoulder elbow real
+
+% End-Effector Pose from IK
+EE = ...
+  dh(a = 0,       d = 0.11518, alpha = pi / 2,  theta = 1.5708) * ...
+  dh(a = -0.013,  d = 0,       alpha = 0,       theta = 0) * ...
+  dh(a = 0.4173,  d = 0,       alpha = 0,       theta = 0.7974) * ...
+  dh(a = 0.48059, d = 0,       alpha = 0,       theta = -0.5961) * ...
+  dh(a = 0.008,   d = 0,       alpha = 0,       theta = pi / 4) * ...
+  dh(a = 0.305,   d = -0.023,  alpha = 0,       theta = -pi / 4 + 0.959931)
+
+% Define Joint Frames
+Base = ...
+  dh(a = 0,       d = 0.11518, alpha = pi / 2,  theta = base) * ...
+  dh(a = -0.013,  d = 0,       alpha = 0,       theta = 0);
+Shoulder = ...
+  dh(a = 0.4173,  d = 0,       alpha = 0,       theta = shoulder);
+Elbow = ...
+  dh(a = 0.48059, d = 0,       alpha = 0,       theta = elbow) * ...
+  dh(a = 0.008,   d = 0,       alpha = 0,       theta = pi / 4) * ...
+  dh(a = 0.305,   d = -0.023,  alpha = 0,       theta = -pi / 4 + 0.959931);
+
+% Define IK Equation
+IK = Base * Shoulder * Elbow == EE;
+
+% Decouple Elbow from Base and Shoulder
+IK = Base * Shoulder == EE * inv(Elbow);
+
+% Solve for Joint Variables
+[base, shoulder, elbow] = solve(IK, [base, shoulder, elbow])
+
+% Print Results
+vpa(base)
+vpa(shoulder)
+vpa(elbow)
