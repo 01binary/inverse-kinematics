7f7f5bb95a4e0ca9badb0fa49b665c7901534c95
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2024-03-20 19:15:39 -0700

    Restore fk

--- exercise16-newton-raphson/fk.h
+++ exercise16-newton-raphson/fk.h
@@ -2,46 +2,31 @@
 
 using namespace Eigen;
 
-enum JOINTS
-{
-  BASE,
-  SHOULDER,
-  ELBOW,
-  WRIST
-};
-
 Matrix4d forwardKinematics(MatrixXd jointVariables)
 {
-  Matrix4d endEffector = Matrix4d::Identity();
-
-  for (int n = 0; n < jointVariables.rows(); n++)
-  {
-    switch (n)
-    {
-      case BASE:
-        endEffector = endEffector * (
-          Translation3d(0, 0, 0) *
-          AngleAxisd(jointVariables(n, 0), Vector3d::UnitZ())
-        ).matrix();
-        break;
-      case SHOULDER:
-        endEffector = endEffector * (
-          Translation3d(0, 0, 0.670) *
-          AngleAxisd(jointVariables(n, 0), Vector3d::UnitY())
-        ).matrix();
-        break;
-      case ELBOW:
-        endEffector = endEffector * (
-          Translation3d(0.7, 0, 0) *
-          AngleAxisd(jointVariables(n, 0), Vector3d::UnitY())
-        ).matrix();
-        break;
-      case WRIST:
-        endEffector = endEffector * (
-          Translation3d(0.7, 0.05, 0) *
-          AngleAxisd(jointVariables(n, 0), Vector3d::UnitX())
-        ).matrix();
-        break;
-    }
-  }
+  return
+    // Base
+    (
+      Translation3d(0, 0, 0) *
+      AngleAxisd(jointVariables(0,0), Vector3d::UnitZ())
+    ).matrix() *
+    // Shoulder
+    (
+      Translation3d(0, 0, 0.670) *
+      AngleAxisd(jointVariables(1,0), Vector3d::UnitY())
+    ).matrix() *
+    // Elbow
+    (
+      Translation3d(0.7, 0, 0) *
+      AngleAxisd(jointVariables(2,0), Vector3d::UnitY())
+    ).matrix() *
+    // Wrist
+    (
+      Translation3d(0.7, 0.05, 0) *
+      AngleAxisd(jointVariables(3,0), Vector3d::UnitX())
+    ).matrix() *
+    (
+      Translation3d(0.18, 0, 0) *
+      AngleAxisd(0.0, Vector3d::UnitX())
+    ).matrix();
 }
